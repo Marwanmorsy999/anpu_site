9969ddbbddca7b2a36efc5ec8134f93f4540d63d
Marawan morsy <m.2396@nu.edu.eg>
2026-08-27 03:38:36 +0300

    feat: load landing hero terminal protocol styles

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ import "./protocol-v7.css"
 import "./protocol-v7-hotfix.css"
 import "./interaction-v8.css"
 import "./live-terminal-v9.css"
+import "./hero-protocol-v10.css"
 import App from "./App.tsx"
 
 createRoot(document.getElementById("root")!).render(
